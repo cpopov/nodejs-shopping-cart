0ade1510b0b2d8201e2aa438cff0c410fc916764
Chris Popov <chrpopov@gmail.com>
2017-02-22 14:32:54 +0000

    failed test

--- tests/basket.spec.js
+++ tests/basket.spec.js
@@ -17,4 +17,14 @@ describe('Basket', function () {
         expect(basket.getTotalCost()).to.equal(2.95);
     });
 
+    it('Given the basket has 2 butter and 2 bread when I total the basket then the total should be £3.10', function () {
+        let basket = new Basket();
+        basket.addItem(new Item('Bread'));
+        basket.addItem(new Item('Bread'));
+        basket.addItem(new Item('Butter'));
+        basket.addItem(new Item('Butter'));
+
+        expect(basket.getTotalCost()).to.equal(3.10);
+    });
+
 });
