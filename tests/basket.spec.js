0c4454a3db2ac03cb913cc7ee857511774bec8e1
Chris Popov <chrpopov@gmail.com>
2017-02-22 15:31:19 +0000

    tests for more discounts

--- tests/basket.spec.js
+++ tests/basket.spec.js
@@ -19,22 +19,25 @@ describe('Basket', function () {
 
     it('Given the basket has 2 butter and 2 bread when I total the basket then the total should be £3.10', function () {
         let basket = new Basket();
-        basket.addItem(new Item('Butter'));
-        basket.addItem(new Item('Butter'));
-        basket.addItem(new Item('Bread'));
-        basket.addItem(new Item('Bread'));
+        basket.addItem(new Item('Butter'),2);
+        basket.addItem(new Item('Bread'),2);
 
         expect(basket.getTotalCost()).to.equal('3.10');
     });
 
     it('Given the basket has 4 milk when I total the basket then the total should be £3.45', function () {
         let basket = new Basket();
-        basket.addItem(new Item('Milk'));
-        basket.addItem(new Item('Milk'));
-        basket.addItem(new Item('Milk'));
-        basket.addItem(new Item('Milk'));
+        basket.addItem(new Item('Milk'),4);
 
         expect(basket.getTotalCost()).to.equal('3.45');
     });
 
+    it('Given the basket has 2 butter, 1 bread and 8 milk when I total the basket then the total should be £9.00', function () {
+        let basket = new Basket();
+        basket.addItem(new Item('Butter'),2);
+        basket.addItem(new Item('Bread'));
+        basket.addItem(new Item('Milk'),8);
+
+        expect(basket.getTotalCost()).to.equal('9.00');
+    });
 });
